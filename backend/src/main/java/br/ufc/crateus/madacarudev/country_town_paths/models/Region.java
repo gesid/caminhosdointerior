e3effb5e1644e0bc7dd1d9590e429c9c664192c4
Alex Mota <alexmota@alu.ufc.br>
2023-07-27 15:58:30 -0300

    ♻️ Refactor: Region use UUID

--- backend/src/main/java/br/ufc/crateus/madacarudev/country_town_paths/models/Region.java
+++ backend/src/main/java/br/ufc/crateus/madacarudev/country_town_paths/models/Region.java
@@ -1,9 +1,10 @@
 package br.ufc.crateus.madacarudev.country_town_paths.models;
 
+import java.util.UUID;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -11,18 +12,15 @@ import javax.persistence.Table;
 @Table
 public class Region {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public Long id;
+    @GeneratedValue
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
 
     @Column(length = 100, nullable = false)
     public String name;
 
-    public Long getId() {
+    public UUID getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
