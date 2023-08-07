e22c0838a10f81aeb569ad6f2c25afdf7e0a716f
Alex Mota <alexmota@alu.ufc.br>
2023-08-06 22:20:27 -0300

    ✨feat: Creating dtos CreateRegion and UpdateRegion

---
+++ backend/src/main/java/br/ufc/crateus/madacarudev/country_town_paths/dtos/input/CreateRegionDto.java
@@ -0,0 +1,10 @@
+package br.ufc.crateus.madacarudev.country_town_paths.dtos.input;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateRegionDto {
+    private String name;
+}
