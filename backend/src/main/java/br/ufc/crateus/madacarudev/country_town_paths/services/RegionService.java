5e0fe03d09bc532555f1ed8fa8da7dd51a812215
Alex Mota <alexmota@alu.ufc.br>
2023-08-19 13:26:25 -0300

    🐛 fix : Correcting so that all logic stays in the service

--- backend/src/main/java/br/ufc/crateus/madacarudev/country_town_paths/services/RegionService.java
+++ backend/src/main/java/br/ufc/crateus/madacarudev/country_town_paths/services/RegionService.java
@@ -2,51 +2,87 @@ package br.ufc.crateus.madacarudev.country_town_paths.services;
 
 import br.ufc.crateus.madacarudev.country_town_paths.dtos.input.CreateRegionDto;
 import br.ufc.crateus.madacarudev.country_town_paths.dtos.input.UpdateRegionDto;
+import br.ufc.crateus.madacarudev.country_town_paths.dtos.output.RegionOutputDto;
+import br.ufc.crateus.madacarudev.country_town_paths.models.CityModel;
 import br.ufc.crateus.madacarudev.country_town_paths.models.RegionModel;
 import br.ufc.crateus.madacarudev.country_town_paths.repositories.RegionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.ufc.crateus.madacarudev.country_town_paths.exceptions.EntityConflictException;
+import br.ufc.crateus.madacarudev.country_town_paths.exceptions.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.Objects;
 
 @Service
 public class RegionService {
 
+    private final ModelMapper modelMapper;
+
+    public RegionService(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
     @Autowired
     private RegionRepository regionRepository;
 
-    public List<RegionModel> getAllRegions() {
-        return regionRepository.findAll();
+    public List<RegionOutputDto> getAllRegions() {
+        List<RegionModel> regionsModel = regionRepository.findAll();
+        List<RegionOutputDto> regionsOutputDto = new ArrayList<RegionOutputDto>();
+
+        for (RegionModel regionModel : regionsModel) {
+            regionsOutputDto.add(modelMapper.map(regionModel,RegionOutputDto.class));
+        }
+
+        return regionsOutputDto;
     }
 
-    public RegionModel getRegionById(UUID id) {
-        return regionRepository.findById(id).orElse(null);
+    public RegionOutputDto getRegionById(UUID id) throws EntityNotFoundException {
+        RegionModel regionModel = regionRepository.findById(id).orElse(null);
+
+        if(Objects.isNull(regionModel)){
+            String errorMessage = "Não existe região com o id: " + id;
+            throw new EntityNotFoundException(errorMessage);
+        }
+
+        RegionOutputDto regionOutputDto = modelMapper.map(regionModel,RegionOutputDto.class);
+
+        return regionOutputDto;
     }
 
     public RegionModel getRegionByName(String name) {
         return regionRepository.findByName(name).orElse(null);
     }
 
-    public RegionModel createRegion(CreateRegionDto region) {
+    public void create(CreateRegionDto region) throws EntityConflictException{
         RegionModel existingRegion = getRegionByName(region.getName());
-        if(existingRegion == null){
-            return null;
+
+        if(Objects.nonNull(existingRegion)){
+            String errorMessage = "Já existe outra região com o nome: " + existingRegion.getName();
+            throw new EntityConflictException(errorMessage);
         }
-        else{
-            RegionModel createdRegion = new RegionModel(region.getName());
-            return regionRepository.save(createdRegion);
-        }
+        UUID uuid = UUID.randomUUID();
+        List<CityModel> cities = new ArrayList<CityModel>();
+        RegionModel regionCreate = new RegionModel(uuid, region.getName(),cities);
+        
+        regionRepository.save(regionCreate);
     }
 
-    public RegionModel updateRegion(UUID id, UpdateRegionDto region) {
+    public void update(UUID id, UpdateRegionDto region) throws EntityNotFoundException  {
         RegionModel existingRegion = regionRepository.findById(id).orElse(null);
         RegionModel updatedRegion = new RegionModel(region.getName());
-        if (existingRegion != null) {
-            existingRegion.setName(updatedRegion.getName());
-            return regionRepository.save(existingRegion);
+
+        if (Objects.isNull(existingRegion)) {
+            String errorMessage = "Não existe região com o id: " + id;
+            throw new EntityNotFoundException(errorMessage);
+
         }
-        return null;
+        existingRegion.setName(updatedRegion.getName());
+        regionRepository.save(existingRegion);
     }
 
     public void deleteRegion(UUID id) {
