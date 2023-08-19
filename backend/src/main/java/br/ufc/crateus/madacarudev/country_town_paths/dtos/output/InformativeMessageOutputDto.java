8aacea1d6ca73fcb2b5be8dade834f1fd31275bd
Alex Mota <alexmota@alu.ufc.br>
2023-08-19 13:17:47 -0300

    ✨ feat:	Creating InformativeMessageOutputDto

---
+++ backend/src/main/java/br/ufc/crateus/madacarudev/country_town_paths/dtos/output/InformativeMessageOutputDto.java
@@ -0,0 +1,14 @@
+package br.ufc.crateus.madacarudev.country_town_paths.dtos.output;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InformativeMessageOutputDto {
+    private String message;
+}
