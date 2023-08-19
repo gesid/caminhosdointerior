44ed548b9bed3b9bc6ffe3c213b9e9a8131b0506
Alex Mota <alexmota@alu.ufc.br>
2023-08-19 13:23:33 -0300

    ➕build: Configuring ModelMapper

---
+++ backend/src/main/java/br/ufc/crateus/madacarudev/country_town_paths/configs/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package br.ufc.crateus.madacarudev.country_town_paths.configs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
