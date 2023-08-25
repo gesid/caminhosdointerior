4f34441671f33f295117e4ce6b29cf58ddb6b5f7
Alex Mota <alexmota@alu.ufc.br>
2023-08-24 23:09:55 -0300

    🐛 fix: adding error handling BadRequestException

--- backend/src/main/java/br/ufc/crateus/madacarudev/country_town_paths/controllers/RegionController.java
+++ backend/src/main/java/br/ufc/crateus/madacarudev/country_town_paths/controllers/RegionController.java
@@ -12,6 +12,7 @@ import br.ufc.crateus.madacarudev.country_town_paths.dtos.input.CreateRegionDto;
 import br.ufc.crateus.madacarudev.country_town_paths.dtos.input.UpdateRegionDto;
 import br.ufc.crateus.madacarudev.country_town_paths.dtos.output.InformativeMessageOutputDto;
 import br.ufc.crateus.madacarudev.country_town_paths.dtos.output.RegionOutputDto;
+import br.ufc.crateus.madacarudev.country_town_paths.exceptions.BadRequestException;
 import br.ufc.crateus.madacarudev.country_town_paths.exceptions.EntityConflictException;
 import br.ufc.crateus.madacarudev.country_town_paths.exceptions.EntityNotFoundException;
 import br.ufc.crateus.madacarudev.country_town_paths.services.RegionService;
@@ -29,27 +30,27 @@ public class RegionController implements RegionControllerOpenApi {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<RegionOutputDto> getRegionById(@PathVariable UUID id) throws EntityNotFoundException {
+    public ResponseEntity<RegionOutputDto> getRegionById(@PathVariable UUID id) throws EntityNotFoundException, BadRequestException{
         RegionOutputDto region = regionService.getRegionById(id);
         return ResponseEntity.ok(region);
     }
 
     @PostMapping
-    public ResponseEntity<InformativeMessageOutputDto> create(@RequestBody CreateRegionDto region) throws EntityConflictException{
+    public ResponseEntity<InformativeMessageOutputDto> create(@RequestBody CreateRegionDto region) throws EntityConflictException, BadRequestException{
         regionService.create(region);
         InformativeMessageOutputDto message = new InformativeMessageOutputDto("Região criada com sucesso");
         return ResponseEntity.status(HttpStatus.CREATED).body(message);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<InformativeMessageOutputDto> update(@PathVariable UUID id, @RequestBody UpdateRegionDto region) throws EntityNotFoundException{
+    public ResponseEntity<InformativeMessageOutputDto> update(@PathVariable UUID id, @RequestBody UpdateRegionDto region) throws EntityNotFoundException, BadRequestException{
         regionService.update(id, region);
         InformativeMessageOutputDto message = new InformativeMessageOutputDto("Região criada com sucesso");
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteRegion(@PathVariable UUID id) {
+    public ResponseEntity<Void> deleteRegion(@PathVariable UUID id) throws BadRequestException {
         regionService.deleteRegion(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
