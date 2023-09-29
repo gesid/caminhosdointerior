315ade6d9c05c026452576ef0104f9deca7dcd23
Samuel Lopes <samuellopes12390@gmail.com>
2023-09-29 19:25:22 -0300

    refactor: Add validations to DTOs for creating and updating cities

--- backend/src/main/java/br/ufc/crateus/madacarudev/country_town_paths/dtos/input/CreateCityInputDto.java
+++ backend/src/main/java/br/ufc/crateus/madacarudev/country_town_paths/dtos/input/CreateCityInputDto.java
@@ -3,6 +3,7 @@ package br.ufc.crateus.madacarudev.country_town_paths.dtos.input;
 import java.util.UUID;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,7 +13,13 @@ import lombok.Setter;
 public class CreateCityInputDto {
     @NotBlank
     private String name;
+
+    @NotBlank
     private String imageBannerUrl;
+
+    @NotBlank
     private String description;
+
+    @NotNull
     private UUID regionId;
 }
