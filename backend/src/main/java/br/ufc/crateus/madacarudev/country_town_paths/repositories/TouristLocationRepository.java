0a02cb19d7d2ff29e74b729e5628e0be9c10d8a2
Alex Mota <alexmota.work@gmail.com>
2023-10-28 20:16:05 -0300

    🐛 fix: Correting use model, TouristLocationModel

--- backend/src/main/java/br/ufc/crateus/madacarudev/country_town_paths/repositories/TouristLocationRepository.java
+++ backend/src/main/java/br/ufc/crateus/madacarudev/country_town_paths/repositories/TouristLocationRepository.java
@@ -1,7 +1,7 @@
 package br.ufc.crateus.madacarudev.country_town_paths.repositories;
 
-import br.ufc.crateus.madacarudev.country_town_paths.models.EventModel;
+import br.ufc.crateus.madacarudev.country_town_paths.models.TouristLocationModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TouristLocationRepository extends JpaRepository<EventModel, Long> {
+public interface TouristLocationRepository extends JpaRepository<TouristLocationModel, Long> {
 }
