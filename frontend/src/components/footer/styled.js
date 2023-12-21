bf249d94ac088dc6650cf415a3fd418bd34cb5a7
Joel Sousa <joe_sousa@alu.ufc.br>
2023-12-21 17:12:41 -0300

    fix: update file style.js to NotFound

--- frontend/src/components/footer/styled.js
+++ frontend/src/components/footer/styled.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 
+export const StyledOverLay = styled.div`
+    width: 100%;
+    height: 3.6rem;
+    z-index: 3;
+    border-bottom: 2px solid ${(props) => props.theme.colors.lightGray};
+    position: absolute;
+`
+
 export const StyledFooter = styled.div`
     background-color: #E4E4D3;
     width: auto;
-    height: 202px;
-    padding:0 5rem;
+    height: 15rem;
+    padding:1.9rem 5rem 0.9rem;
     font-size: 1rem;
     color: black;
     display: grid;
@@ -13,19 +21,25 @@ export const StyledFooter = styled.div`
     h5{
         margin-top:auto;
         text-decoration: solid;
+        font-size: 1rem;
     }
 ;
 
 `
 export const StyledList = styled.div`
-    gap:1rem;
+    gap:1.25rem;
     flex-direction: column;
-    font-size: 1rem;
+    font-size: 1.25rem;
     color: black;
     display: flex;
 
+    h6{
+        font-size: 1.25rem;
+    }
+
     li{ 
         list-style: none;
+        font-size: 1rem;
     }
 ;
 `
@@ -34,12 +48,45 @@ export const StyledSocialMedia = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 1rem;
+    
+    h6{
+        font-size: 1.25rem;
+    }
+
+    gap: 1.25rem;
     .logo{
         display:flex;
-        gap:0.5rem;
+        gap:0.6rem;
      }
 `
 
 
+export const StyledNotFoundPageText1 = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 18.75rem;
+    
+    .text1{
+        width: 35.90rem;
+        height: 9.37rem;
+    }
+`
 
+export const StyledNotFoundPageText2 = styled.div`
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 20.62rem;
+    margin-left:3rem;
+    
+    .text2{
+        width: 41.31rem;
+        height: 18.75rem;
+    }
+`
+export const StyledNotFoundImage = styled.img`
+    width: 84rem;
+    height: 35rem;  
+`
